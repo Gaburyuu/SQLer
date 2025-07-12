b35eb97f8ea38385c0a9c61370f1987da72dbdfb
Gabu <gaburyuu@gmail.com>
2025-07-12 09:24:30 +0900

    wrote some tests

--- tests/sync/test_expression.py
+++ tests/sync/test_expression.py
@@ -22,3 +22,49 @@ def test_and():
     assert combined.sql == f"({LEN_SQL}) AND ({TM_SQL})"
     # same with .params
     assert combined.params == [20, 50]
+
+
+def test_or():
+    """tests or to combine expresions"""
+    # try to combine some sql expressions
+    a = SQLerExpression(LEN_SQL, [20])
+    b = SQLerExpression(TM_SQL, [50])
+    # gonna make sure it can combine two expressions
+    combined = a | b
+
+    # should have a .sql that we can inspect
+    assert combined.sql == f"({LEN_SQL}) OR ({TM_SQL})"
+    # same with .params
+    assert combined.params == [20, 50]
+
+
+def test_not():
+    """tests not with ~"""
+    # try to negate the expression
+    a = SQLerExpression(IS_SQL)
+    negated = ~a
+
+    # should have a .sql that we can inspect
+    assert negated.sql == f"NOT ({IS_SQL})"
+    # same with .params
+    assert negated.params == []
+
+
+def test_str():
+    """tests string representation"""
+    a = SQLerExpression(LIKE_SQL, ["G%G"])
+    assert str(a) == LIKE_SQL
+    assert LIKE_SQL in repr(a)
+    assert "'G%G'" in repr(a)
+
+
+def test_chaining():
+    """tests chaining more than one"""
+    # let's try and combine these
+    a = SQLerExpression(LEN_SQL, [20])
+    b = SQLerExpression(TM_SQL, [50])
+    c = SQLerExpression(LIKE_SQL, ["TTT%"])
+
+    expression = (a | b) & c
+    assert expression.sql == f"(({LEN_SQL}) OR ({TM_SQL}) OR ({LIKE_SQL}))"
+    assert expression.params == [20, 50, "TTT%"]
