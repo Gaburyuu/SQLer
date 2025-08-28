efde621af7021a3468612aeced424d2967c1016a
Gabu <gaburyuu@gmail.com>
2025-08-28 13:32:51 +0900

    battling perf bug

--- tests/perf/conftest.py
+++ tests/perf/conftest.py
@@ -5,10 +5,14 @@ def pytest_addoption(parser):
     parser.addoption("--perf", action="store_true", help="run performance tests")
 
 
+from pathlib import Path
+
+
 def pytest_collection_modifyitems(config, items):
     if getattr(config.option, "perf", False):
         return
     skip_perf = pytest.mark.skip(reason="add --perf to run performance tests")
     for item in items:
-        if "tests/perf/" in str(item.fspath):
+        parts = Path(str(item.fspath)).parts
+        if "perf" in parts:
             item.add_marker(skip_perf)
